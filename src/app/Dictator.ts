9bef949dae19300bf1644dca364172f95ddcc94c
Benjamin Damkjær Østner <benj4539@zbc.dk>
2020-08-12 11:50:44 +0200

    Upload

---
+++ src/app/Dictator.ts
@@ -0,0 +1,14 @@
+export class Dictator{
+
+    name: string;
+    lastname: string;
+    dateOfBirth: string;
+    comment: string;
+
+    constructor(name, lastname, dateOfBirht, comment){
+        this.name = name;
+        this.lastname = lastname
+        this.dateOfBirth = dateOfBirht;
+        this.comment = comment;
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { Dictator } from './Dictator'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DictatorAngular';
+export class AppComponent implements OnInit {
+
+  ngOnInit(){
+    this.dictatorData = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      lastname: new FormControl('', [Validators.required]),
+      dateOfBirth: new FormControl('', [Validators.required]),
+      comment: new FormControl('')
+    });
+  }
+
+  dictatorData:FormGroup;
+  dictators: Array<Dictator> = Array<Dictator>();
+
+  onNewDictatorSubmit() {
+    if (this.dictatorData.valid)
+    {
+      console.log(this.dictatorData.value);
+      this.dictators.push(this.dictatorData.value);
+    }
+  }
+
+  deleteThis(){
+    if (this.dictatorData.valid)
+    {
+        for(let counter = 0; counter < this.dictators.length; counter++)
+      {
+        if((this.dictatorData.get("name").value == this.dictators[counter].name) && 
+        (this.dictatorData.get("lastname").value == this.dictators[counter].lastname) && 
+        (this.dictatorData.get("dateOfBirth").value == this.dictators[counter].dateOfBirth))
+        {
+          this.dictators.splice(this.dictators.indexOf(this.dictators[counter]), 1)
+          break;
+        }
+      }
+    }
+  }
 }
